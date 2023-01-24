d7b18af70212e58fee3f3e618d3a553654fdf32e
Fabro2701 <55029358+Fabro2701@users.noreply.github.com>
2023-01-24 17:52:18 +0100

    min and max functions to locate general points(for grammar)

--- PaperPlaneEvolution/src/main/java/model/BasicPlane.java
+++ PaperPlaneEvolution/src/main/java/model/BasicPlane.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import util.Triangle;
 import util.Vector3D;
@@ -12,6 +13,12 @@ public class BasicPlane extends AbstractPlane{
 	public Vector3D upperLeftCorner, upperMiddleLeft;
 	
 	List<Triangle>tris;
+	List<PlaneShape>shapes;
+	
+	private BasicPlane() {
+		this.tris = new ArrayList<>();
+		this.shapes = new ArrayList<>();
+	}
 	public static BasicPlane construct(float baseLength, float backUpperCornerShift, float bodyAngle ,float bodyHeight) {
 		BasicPlane plane = new BasicPlane();
 		
@@ -29,12 +36,51 @@ public class BasicPlane extends AbstractPlane{
 											plane.startBase.z - bodyAngle);
 		plane.upperMiddleLeft = Vector3D.mul(Vector3D.add(plane.endBase, plane.upperLeftCorner), 0.5f);
 		
-		plane.tris = new ArrayList<Triangle>();
-		plane.tris.add(Triangle.of(plane.startBase, plane.endBase, plane.upperRightCorner));
-		plane.tris.add(Triangle.of(plane.startBase, plane.endBase, plane.upperLeftCorner));
+		
+		
+		PlaneShape base1 = new BasicPlaneShape(plane.startBase, plane.upperRightCorner, plane.endBase);
+		PlaneShape base2 = new BasicPlaneShape(plane.startBase, plane.upperLeftCorner, plane.endBase);
+		plane.shapes.add(base1);
+		plane.shapes.add(base2);
+		plane.tris.addAll(base1.getTriangles());
+		plane.tris.addAll(base2.getTriangles());
 		return plane;
 	}
+	
+	private float max(Function<Vector3D, Float>f) {
+		Triangle t = this.tris.stream().max((Triangle t1, Triangle t2)->{float m1=Math.max(f.apply(t1.points[0]), Math.max(f.apply(t1.points[1]), f.apply(t1.points[2])));
+																		 float m2=Math.max(f.apply(t2.points[0]), Math.max(f.apply(t2.points[1]), f.apply(t2.points[2])));
+																		 return Float.compare(m1,m2);}
+											).get();
+		return Math.max(f.apply(t.points[0]), Math.max(f.apply(t.points[1]), f.apply(t.points[2])));
+	}
+	private float min(Function<Vector3D, Float>f) {
+		Triangle t = this.tris.stream().min((Triangle t1, Triangle t2)->{float m1=Math.min(f.apply(t1.points[0]), Math.min(f.apply(t1.points[1]), f.apply(t1.points[2])));
+																		 float m2=Math.min(f.apply(t2.points[0]), Math.min(f.apply(t2.points[1]), f.apply(t2.points[2])));
+																		 return Float.compare(m1,m2);}
+											).get();
+		return Math.min(f.apply(t.points[0]), Math.min(f.apply(t.points[1]), f.apply(t.points[2])));
+	}
+	public float maxX() {
+		return max((Vector3D v)->v.x);
+	}
+	public float maxY() {
+		return max((Vector3D v)->v.y);
+	}
+	public float maxZ() {
+		return max((Vector3D v)->v.z);
+	}
+	public float minX() {
+		return min((Vector3D v)->v.x);
+	}
+	public float minY() {
+		return min((Vector3D v)->v.y);
+	}
+	public float minZ() {
+		return min((Vector3D v)->v.z);
+	}
 	public void addShape(PlaneShape shape) {
+		shapes.add(shape);
 		tris.addAll(shape.getTriangles());
 	}
 	@Override
@@ -59,6 +105,7 @@ public class BasicPlane extends AbstractPlane{
 	public static void main(String args[]) {
 		BasicPlane plane = BasicPlane.construct(50f, 10f, 10f, 15f);
 		System.out.println(plane);
+		System.out.println(plane.maxX());
 	}
 	
 }
