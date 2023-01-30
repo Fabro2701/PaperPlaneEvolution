efea83705c5097cf3829960d8b26db84351f33b7
Fabro2701 <55029358+Fabro2701@users.noreply.github.com>
2023-01-31 00:36:13 +0100

    wind ray simulation and degrees to neighbor spread

--- PaperPlaneEvolution/src/main/java/Main.java
+++ PaperPlaneEvolution/src/main/java/Main.java
@@ -10,12 +10,13 @@ import util.Vector3D;
  */
 public class Main  {
 	public static void main(String args[]) {
-		System.out.println(intersectionArea(Vector3D.of(0, 1, 0),
-									Vector3D.of(5, 5, 5),
-									Vector3D.of(1, 0, 0),
-								Vector3D.of(2, 1, 0.5)));
+		System.out.println(intersectionArea(Vector3D.of(0, 0, 0),
+											Vector3D.of(0, 10, 3.6),
+											Vector3D.of(40, 0, 0),
+								Vector3D.of(1, 0, 0.1),
+								Vector3D.of(0, 1, 0)));
 	}
-	public static boolean intersectionArea(Vector3D p0, Vector3D p1, Vector3D p2, Vector3D vector) {
+	public static boolean intersectionArea(Vector3D p0, Vector3D p1, Vector3D p2, Vector3D origin, Vector3D vector) {
 
 	    Vector3D v0 = Vector3D.sub(p1, p0);
 		Vector3D v1 = Vector3D.sub(p2, p1);
@@ -23,16 +24,14 @@ public class Main  {
 	    Vector3D normal = Vector3D.crossProduct(v0, v1);
 	    normal = Vector3D.normal(normal);
 	    
-	    Vector3D O = Vector3D.of(0, 0, 0);
+	    Vector3D O = origin;
 	    Vector3D R = Vector3D.normal(vector);
 
 	    double D = -Vector3D.dotProduct(normal, p1);
 	    double t = -(D + Vector3D.dotProduct(normal, O))/(Vector3D.dotProduct(normal, R));
 	    
-	    //double t = -(normal.x * vector.x + normal.y * vector.y + normal.z * vector.z + d) / Vector3D.dotProduct(normal, vector);
-
 	    Vector3D phit = Vector3D.add(O, Vector3D.mul(R, t));
-	    if (t < 0d) {
+	    if (Math.abs(t) < 0.0001d) {
 	      return false;
 	    }
 
@@ -45,22 +44,7 @@ public class Main  {
 	       Vector3D.dotProduct(normal, Vector3D.crossProduct(v1, c1))>0d &&
 	       Vector3D.dotProduct(normal, Vector3D.crossProduct(v2, c2))>0d)b =  true;
 
-	    
-	    
 	    return b;
-//	    double dot00 = Vector3D.dotProduct(v0,v0);
-//	    double dot01 = Vector3D.dotProduct(v0,v1);
-//	    double dot02 = Vector3D.dotProduct(v0,v2);
-//	    double dot11 = Vector3D.dotProduct(v1,v1);
-//	    double dot12 = Vector3D.dotProduct(v1,v2);
-//	    double invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
-//	    double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-//	    double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-//
-//	    if ((u >= 0) && (v >= 0) && (u + v <= 1)) {
-//	      return Vector3D.length(v0) * Vector3D.length(normal) * invDenom * 0.5d;
-//	    } else {
-//	      return 0;
-//	    }
+
 	  }
 }
--- PaperPlaneEvolution/src/main/java/model/engine/Engine.java
+++ PaperPlaneEvolution/src/main/java/model/engine/Engine.java
@@ -11,13 +11,15 @@ import util.Vector3D;
 
 public class Engine {
 	BasicPlane plane;
-	final double GRAVITY = 0.001d;
+	final double GRAVITY = 0.0001d;
 	
 	public Engine(BasicPlane plane) {
 		this.plane = plane;
 	}
 	public void step(double dt) {
 		List<Triangle> tris = this.plane.getTriangles();
+		Vector3D origin = Vector3D.of(5, 0, 5);
+		Vector3D wind = Vector3D.of(0, 1, 0);
 		
 		//double sumMass = tris.stream().mapToDouble((Triangle t)->{return t.mass;}).sum();
 		PointMap neighbors = plane.getNeighMap();
@@ -28,51 +30,72 @@ public class Engine {
 				tri.tmpforce[i].reset();
 			}
 		}
+		
+		for(Triangle tri:tris) {
+			Vector3D phit = intersectionTriangle(tri.points[0],tri.points[1],tri.points[2],origin,wind);
+			if(phit!=null) {
+				double d[] = new double[3];
+				double sum = 0d;
+				for(int i=0;i<3;i++) {
+					d[i] = Math.abs(Vector3D.length(Vector3D.sub(phit, tri.points[i])));
+					sum += d[i];
+					
+				}
+				for(int i=0;i<3;i++) {
+					d[i] = 1d - (d[i]/sum);
+					tri.force[i].add(Vector3D.mul(wind, d[i]*0.1d));
+				}
+			}
+			
+		}
 		for(Triangle tri:tris) {
 			//System.out.println(neighbors.size());
 			for(int i=0;i<3;i++) {
 				tri.force[i].y += -GRAVITY*tri.mass;
 			}
 		}
-		for(Triangle tri:tris) {
-			//System.out.println(neighbors.size());
-			for(int i=0;i<3;i++) {
-				tri.force[i].y += -GRAVITY*tri.mass;
+//		System.out.println("before");
+//		for(Triangle tri:tris) {
+//			for(int i=0;i<3;i++) {
+//				System.out.print(tri.force[i]+"  ");
+//			}
+//			System.out.println("");
+//		}
+		for(int d=0;d<2;d++) {
+			for(Triangle tri:tris) {
+				for(int i=0; i<3; i++) {
+
+					tri.tmpforce[i].div(Math.pow(3, d));
+					
+				}
+			}
+			for(Triangle tri:tris) {
+				for(int i=0;i<3;i++) {
+					if(!neighbors.containsKey(tri.points[i]))continue;
+					spreadToNeighbors(tri.points[i], tri.force[i], neighbors.get(tri.points[i]));
+				}
+			}
+			for(Triangle tri:tris) {
+				for(int i=0; i<3; i++) {
+					tri.force[i].add(tri.tmpforce[i]);
+					tri.tmpforce[i].reset();
+		        }
 			}
 		}
-		System.out.println("before");
-		for(Triangle tri:tris) {
-			for(int i=0;i<3;i++) {
-				System.out.print(tri.force[i]+"  ");
-			}
-			System.out.println("");
-		}
-			
-		for(Triangle tri:tris) {
-			for(int i=0;i<3;i++) {
-				if(!neighbors.containsKey(tri.points[i]))continue;
-				spreadToNeighbors(tri.points[i], tri.force[i], neighbors.get(tri.points[i]));
-			}
-		}
-		for(Triangle tri:tris) {
-			
-			for(int i=0; i<3; i++) {
-				tri.force[i].add(tri.tmpforce[i]);
-	        }
-		}
+		
 		for(Triangle tri:tris) {
 			for(int i=0; i<3; i++) {
 				tri.points[i].add(Vector3D.add(Vector3D.add(tri.force[i], dt), dt));
 	        }
 		}
 		
-		System.out.println("after");
-		for(Triangle tri:tris) {
-			for(int i=0;i<3;i++) {
-				System.out.print(tri.force[i]+"  ");
-			}
-			System.out.println("");
-		}
+//		System.out.println("after");
+//		for(Triangle tri:tris) {
+//			for(int i=0;i<3;i++) {
+//				System.out.print(tri.force[i]+"  ");
+//			}
+//			System.out.println("");
+//		}
 	}
 	
 	private void spreadToNeighbors(Vector3D point, Vector3D force, List<Vector3D> list) {
@@ -82,7 +105,36 @@ public class Engine {
 				//t.force[idx].add(-GRAVITY*tri.mass*0.05);
 			
 	}
-	private void dragNeighbors(Triangle tri, List<Triangle> neighbors, Vector3D change) {
-		
-	}
+
+	public static Vector3D intersectionTriangle(Vector3D p0, Vector3D p1, Vector3D p2, Vector3D origin, Vector3D vector) {
+
+	    Vector3D v0 = Vector3D.sub(p1, p0);
+		Vector3D v1 = Vector3D.sub(p2, p1);
+	    Vector3D v2 = Vector3D.sub(p0, p2);
+	    Vector3D normal = Vector3D.crossProduct(v0, v1);
+	    normal = Vector3D.normal(normal);
+	    
+	    Vector3D O = origin;
+	    Vector3D R = Vector3D.normal(vector);
+
+	    double D = -Vector3D.dotProduct(normal, p1);
+	    double t = -(D + Vector3D.dotProduct(normal, O))/(Vector3D.dotProduct(normal, R));
+	    
+	    Vector3D phit = Vector3D.add(O, Vector3D.mul(R, t));
+	    if (Math.abs(t) < 0.0001d) {
+	      return null;
+	    }
+
+	    Vector3D c0 = Vector3D.sub(phit, p0);
+	    Vector3D c1 = Vector3D.sub(phit, p1);
+	    Vector3D c2 = Vector3D.sub(phit, p2);
+	    
+	    
+	    if(!(Vector3D.dotProduct(normal, Vector3D.crossProduct(v0, c0))>0d &&
+	       Vector3D.dotProduct(normal, Vector3D.crossProduct(v1, c1))>0d &&
+	       Vector3D.dotProduct(normal, Vector3D.crossProduct(v2, c2))>0d))return null;
+
+	    return phit;
+
+	  }
 }
